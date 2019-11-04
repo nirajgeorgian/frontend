98ae043f41fa3df5699d7bc67be6a4169c23947b
niraj georgian <nirajgeorgian@gmail.com>
2019-11-04 22:44:59 +0530

    added production script

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,12 @@
 import { InMemoryCache, ApolloClient, HttpLink } from '@apollo/client'
 
 const { REACT_APP_CIRCLES_URI, NODE_ENV } = process.env
-const GraphQLLink = REACT_APP_CIRCLES_URI ? REACT_APP_CIRCLES_URI : 'http://api.joincircles.co'
+const GraphQLLink = REACT_APP_CIRCLES_URI || 'http://api.joincircles.co'
 
 const client = new ApolloClient({
 	link: new HttpLink({ uri: GraphQLLink }),
 	cache: new InMemoryCache({
-		addTypename: NODE_ENV === 'development' ? true : false
+		addTypename: NODE_ENV === 'development'
 	}),
 	connectToDevTools: true
 })
